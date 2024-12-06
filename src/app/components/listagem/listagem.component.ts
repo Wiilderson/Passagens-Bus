610db0e302157c7ecf7daae3f70f829471cc18c5
wilderson <wilderson@uitecnologia.com.br>
2024-12-06 11:04:48 -0300

    component listagem

--- src/app/components/listagem/listagem.component.ts
+++ src/app/components/listagem/listagem.component.ts
@@ -1,43 +1,35 @@
 import { Component } from '@angular/core';
 import { SearchTicketsService } from '../../services/search-tickets.service';
 import { horariosTickets } from '../../../Data/data';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ScheduleResult } from '../../DTO/passagem.dto';
 
 @Component({
   selector: 'app-listagem',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './listagem.component.html',
   styleUrl: './listagem.component.scss',
 })
 export class ListagemComponent {
   schedulesData: any = {};
   filteredSchedules: any[] = [];
+  schedulesResults: ScheduleResult[] = [];
 
   constructor(private searchTickets: SearchTicketsService) {}
 
   ngOnInit() {
     this.schedulesData = this.searchTickets.getSearchData();
+    this.schedulesResults = this.searchTickets.getSearchDataResults();
 
+    console.log('Resultados da pesquisa:', this.schedulesData);
+    console.log('Resultados da pesquisa:', this.schedulesResults);
     if (!this.schedulesData) {
       // Redireciona para a página de busca se os dados não estiverem definidos
       alert('Nenhuma pesquisa realizada. Retornando à busca.');
       window.location.href = '/';
       return;
     }
-
-    this.loadSchedules();
-  }
-
-  loadSchedules() {
-    const { origin, destination } = this.schedulesData;
-
-    // Filtra os horários simulados com base na origem e destino
-    const match = horariosTickets.find(
-      (item) =>
-        item.origin.toLowerCase() === origin.toLowerCase() &&
-        item.destination.toLowerCase() === destination.toLowerCase()
-    );
-
-    this.filteredSchedules = match ? match.schedules : [];
   }
 }
